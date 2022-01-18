e84ef4e8a84af7c9ed15bcabb9827f7d13bac26e
Spotexx <travesimo@gmail.com>
2022-01-18 16:41:57 -0500

    created generatePassword and setPasswordLength function

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ let passwordLength = 0;
 let isSybolsAllowed = false;
 let isNumbersAllowed = false;
 
+let setPasswordLength = () => {
+  passwordLength = prompt('Length\nHow many characters would you like your password to be?\n(type an integer between 8-128)');
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    passwordLength = parseInt(passwordLength);
+    return(passwordLength);
+  } else {
+    alert("Password length must be an integer between 8 and 128");
+    setPasswordLength();
+  }
+}
+
+let generatePassword = () => {
+  setPasswordLength();
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
