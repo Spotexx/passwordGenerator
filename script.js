be209b408ed024776355de40c9087a31d38f7953
Spotexx <travesimo@gmail.com>
2022-01-18 17:05:12 -0500

    added setAvailableCharacters function to generate password function

--- script.js
+++ script.js
@@ -34,11 +34,12 @@ let setAvailableCharacters = () => {
   availableCharacters.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
   availableCharacters.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
   if(isSymbolsAllowed) {
-    availableCharacters.push("!","@","#","$","%","^","&","*","(",")","-","_","+","=","[","]","{","}",";",":","'","\"",",","<",".",">","/","?");
+    availableCharacters.push("!","@","#","$","%","^","&","*","-","_","+","=","[","]","{","}",";",":","'",",","<",".",">","/","?");
   }
   if(isNumbersAllowed) {
     availableCharacters.push("0","1","2","3","4","5","6","7","8","9");
   }
+  return
 }
 
 //pulls all of the functions together to generate the password
@@ -47,6 +48,7 @@ let generatePassword = () => {
   setIsSymbolsAllowed();
   setIsNumbersAllowed();
   console.log(passwordLength + " " + isSybolsAllowed + " " + isNumbersAllowed);
+  setAvailableCharacters();
 }
 
 // Write password to the #password input
